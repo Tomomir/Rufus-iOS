2bc15b8dd1ab68ae3445d908cd7063986a8304bb
Tomáš Pecuch <tomaspecuch14@gmail.com>
2018-10-16 18:24:34 +0200

    -init

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Tomas Pecuch on 16/10/2018.
+//  Copyright © 2018 Touch Art. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
