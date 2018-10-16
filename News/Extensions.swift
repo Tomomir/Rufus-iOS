87d58efcf64dcb924ef68cd5fa0789532554d557
Tomáš Pecuch <tomaspecuch14@gmail.com>
2018-10-16 20:01:47 +0200

    Config file added

---
+++ News/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  News
+//
+//  Created by Tomas Pecuch on 16/10/2018.
+//  Copyright © 2018 Touch Art. All rights reserved.
+//
+
+import Foundation
---
+++ News/RootVC.swift
@@ -0,0 +1,30 @@
+//
+//  RootVC.swift
+//  News
+//
+//  Created by Tomas Pecuch on 16/10/2018.
+//  Copyright © 2018 Touch Art. All rights reserved.
+//
+
+import UIKit
+
+class RootVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
